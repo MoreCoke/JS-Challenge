c521ca7bd377202644240d13fca7302ef18f93e3
MoreCoke <52279841+MoreCoke@users.noreply.github.com>
2019-09-18 00:06:19 +0800

    Update all.js

--- all.js
+++ all.js
@@ -24,19 +24,19 @@ var clockSecond = document.querySelector(".clock-second");
 //     runHour();
 // }, 1000);
 
-function runSecond() {
-    var dayObj = new Date();
-    var second = dayObj.getSeconds();
-    clockSecond.style = `transform: rotate(${second * 6+180}deg) translateX(-50%);`
-}
-function runMinute(){
-    var dayObj = new Date();
-    var minute = dayObj.getMinutes(); 
-    clockMinute.style = `transform: rotate(${minute * 6+180}deg) translateX(-50%);`
-}
-function runHour(){
-    var dayObj = new Date();
-    var hour = dayObj.getHours();
-    var minute =dayObj.getMinutes()/2;   //每分鐘轉0.5度
-    clockHour.style = `transform: rotate(${hour*30+minute+180}deg) translateX(-50%);`  //一小時轉30度
-}
+// function runSecond() {
+//     var dayObj = new Date();
+//     var second = dayObj.getSeconds();
+//     clockSecond.style = `transform: rotate(${second * 6+180}deg) translateX(-50%);`
+// }
+// function runMinute(){
+//     var dayObj = new Date();
+//     var minute = dayObj.getMinutes(); 
+//     clockMinute.style = `transform: rotate(${minute * 6+180}deg) translateX(-50%);`
+// }
+// function runHour(){
+//     var dayObj = new Date();
+//     var hour = dayObj.getHours();
+//     var minute =dayObj.getMinutes()/2;   //每分鐘轉0.5度
+//     clockHour.style = `transform: rotate(${hour*30+minute+180}deg) translateX(-50%);`  //一小時轉30度
+// }
