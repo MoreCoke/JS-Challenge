9be7a9da3a205138768c9e55d8f22098cf97af7b
William Chen <seed45699@gmail.com>
2020-03-24 22:20:07 +0800

    練習用json接資料

--- B4時區/done_with_js/all.js
+++ B4時區/done_with_js/all.js
@@ -1,51 +1,40 @@
 var timetable = document.querySelector(".timetable");
-var country = [
-    {
-        city: 'NEW YORK',
-        timeZone: 'America/New_York',
-    },
-    {
-        city: 'LONDON',
-        timeZone: 'Europe/London',
-    },
-    {
-        city: 'BANGKOK',
-        timeZone: 'Asia/Bangkok',
-    },
-    {
-        city: 'TAIWAN',
-        timeZone: 'Asia/Taipei',
-    },
-    {
-        city: 'SYDNEY',
-        timeZone: 'Australia/Sydney',
-    },
-];
-
-function getData() {
-    var setting = { day: 'numeric', month: 'short', year: 'numeric', hour12: false, minute: 'numeric', hour: 'numeric' };
-    var str = '';
-    country.forEach(function (element) {
-        var option = { timeZone: element.timeZone, ...setting };
-        var arr = new Date().toLocaleDateString('en-GB', option).split(" ");
-        var currentDay = arr.slice(0, 3).splice(1, 0, " ").splice(3, 0, ".");//取arr中日期的值
-        var date = currentDay.join("").replace(/\,/g, "");
-        var time = arr.slice(3).join('');
-        str += `
-        <div class="d-flex justify-content-between align-items-center px-4 py-3">
-            <div>
-                <h2>${element.city}</h2>
-                <span>${date}</span>
-            </div>
-            <div class="time">${time}</div>
-        </div>
-        `
-    });
-    return str;
-};
-
 function showData() {
-    timetable.innerHTML = getData();
+    var country = [];
+    var xhr = new XMLHttpRequest();
+    const url = "./country.json";
+    xhr.open("get", url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            country = JSON.parse(xhr.response).data;
+            //foreach設定各國的new Date物件後，再製作模板
+            function setTemp() {
+                var setting = { day: 'numeric', month: 'short', year: 'numeric', hour12: false, minute: 'numeric', hour: 'numeric' };
+                var str = '';
+                var temp = function (element) {
+                    var option = { timeZone: element.timeZone, ...setting };
+                    var arr = new Date().toLocaleDateString('en-GB', option).split(" ");
+                    var currentDay = arr.slice(0, 3).splice(1, 0, " ").splice(3, 0, ".");//取arr中日期的值
+                    var date = currentDay.join("").replace(/\,/g, "");
+                    var time = arr.slice(3).join('');
+                    str += `
+            <div class="d-flex justify-content-between align-items-center px-4 py-3">
+                <div>
+                    <h2>${element.city}</h2>
+                    <span>${date}</span>
+                </div>
+                <div class="time">${time}</div>
+            </div>
+            `
+                }
+                country.forEach(temp);
+                return str;
+            };
+            timetable.innerHTML = setTemp();
+        } else {
+            console.log("error");
+        }
+    }
+    xhr.send();
 };
-
-setInterval(showData,1000);
+setInterval(showData, 1000);
